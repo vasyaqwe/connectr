2d1caa5a244c1e864c27c8c3fffb4a1d7bc1d08f
vasyaqwe <vasyaqwe>
2023-08-23 18:46:51 +0300

    feat: added watermark

---
+++ README.md
@@ -0,0 +1,26 @@
+A MERN social media app.
+
+Stack used to build this project:
+
+server:
+
+typescript
+nodejs
+express
+mongoose
+cloudinary for image upload
+zod
+
+client:
+
+vite
+typescript
+react
+zod
+tailwind
+
+Server deployed on render.com | client deployed on netlify.com
+
+Live demo - https://connectr-vasyaqwe.netlify.app/
+
+You can find me here - https://www.upwork.com/freelancers/~015c1b113a62e11b13
--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import { useStore } from "@/stores/useStore"
 import { Toast } from "@/components/ui/Toast"
 import { useEffect } from "react"
@@ -21,6 +21,26 @@ export const Layout = () => {
         <>
             {user && <Header />}
             <main className="container mx-auto mt-8 sm:mt-12">
+                <p className="fixed p-2 text-sm -translate-x-1/2 rounded-md bg-primary-800 bottom-4 left-1/2">
+                    Created by{" "}
+                    <Link
+                        className="link"
+                        target="_blank"
+                        to={
+                            "https://www.upwork.com/freelancers/~015c1b113a62e11b13"
+                        }
+                    >
+                        Vasyl P
+                    </Link>
+                    . Source code available on{" "}
+                    <Link
+                        target="_blank"
+                        className="link"
+                        to="https://github.com/vasyaqwe/connectr"
+                    >
+                        GitHub
+                    </Link>
+                </p>
                 <Toast />
                 <Outlet />
             </main>
