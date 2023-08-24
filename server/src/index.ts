053d54839b640f4c2cbde354e0ca0a0f04741c99
vasyaqwe <vasyaqwe>
2023-08-24 14:23:51 +0300

    chore: added netlify.toml & update base urls

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ const app = express()
 connectDB()
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://connectr.vercel.app"],
+    origin: ["http://localhost:5173", "https://connectr.netlify.app"],
     credentials: true,
 }
 
